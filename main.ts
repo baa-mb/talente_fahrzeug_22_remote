a035bebd5a221649d906c64d1f1e95ab8ae4b0c4
baa <34665120+baa-mb@users.noreply.github.com>
2026-06-06 14:44:26 +0200

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,20 +1,20 @@
+input.onButtonPressed(Button.B, function () {
+    if (heben) {
+        radio.sendValue("kupplung", 1)
+    } else {
+        radio.sendValue("kupplung", 0)
+    }
+    heben = !(heben)
+})
 let kurve = 0
 let gerade = 0
+let heben = false
 let alt_gerade = -99
 let alt_kurve = -99
 radio.setGroup(26)
 basic.showString("U26")
-// basic.showArrow(ArrowNames.South)
-basic.showString("<-")
 let g_empfind = 5
 let k_empfind = 5
-basic.showLeds(`
-    . . # . .
-    . # . . .
-    # . # # #
-    . # . . .
-    . . # . .
-    `)
 basic.showLeds(`
     . . . . .
     . . . . .
@@ -38,20 +38,16 @@ basic.forever(function () {
     kurve = Math.min(Math.max(input.rotation(Rotation.Roll), -45), 45)
     kurve = Math.round(kurve / k_empfind) * k_empfind
     kurve = Math.round(kurve / empfind * 2) * empfind * 2
-    // basic.pause(100)
-    if (input.buttonIsPressed(Button.A)) {
-
-
-        if (gerade != alt_gerade || kurve != alt_kurve) {
+    if (gerade != alt_gerade || kurve != alt_kurve) {
+        // basic.pause(100)
+        if (input.buttonIsPressed(Button.A)) {
             radio.sendValue("gerade", gerade)
             radio.sendValue("kurve", kurve / 3)
-            alt_gerade = gerade
-            alt_kurve = kurve
-        }
-    } else {
+        } else {
             radio.sendValue("gerade", 0)
             radio.sendValue("kurve", 0)
-
-
+        }
+        alt_gerade = gerade
+        alt_kurve = kurve
     }
 })
