241394beb2eead90480dbadd0d0adc77cff12bf9
baa <34665120+baa-mb@users.noreply.github.com>
2026-06-06 17:07:44 +0200

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,18 +1,18 @@
 input.onButtonPressed(Button.B, function () {
-    if (heben) {
+    if (oben) {
         radio.sendValue("kupplung", 1)
     } else {
         radio.sendValue("kupplung", 0)
     }
-    heben = !(heben)
+    oben = !(oben)
 })
 let kurve = 0
 let gerade = 0
-let heben = false
+let oben = false
 let alt_gerade = -99
 let alt_kurve = -99
 radio.setGroup(26)
-basic.showString("U26")
+basic.showString("26")
 let g_empfind = 5
 let k_empfind = 5
 basic.showLeds(`
@@ -26,15 +26,7 @@ let empfind = 4
 basic.forever(function () {
     gerade = Math.min(Math.max(input.rotation(Rotation.Pitch), -45), 45)
     gerade = Math.round(gerade / k_empfind) * k_empfind
-    // if (gerade < 0)
-    // basic.showArrow(ArrowNames.North)
-    // else if (gerade > 0)
-    // basic.showArrow(ArrowNames.South)
-    // if (kurve < 0)
-    // basic.showArrow(ArrowNames.West)
-    // else if (kurve > 0)
-    // basic.showArrow(ArrowNames.East)
-    // basic.pause(100)
+
     kurve = Math.min(Math.max(input.rotation(Rotation.Roll), -45), 45)
     kurve = Math.round(kurve / k_empfind) * k_empfind
     kurve = Math.round(kurve / empfind * 2) * empfind * 2
